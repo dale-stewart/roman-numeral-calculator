d2492a3fd0f0e40f13fd77190a4f71cbceb27f75
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 16:00:30 -0500

    Make I+VI test pass

--- roman.c
+++ roman.c
@@ -11,8 +11,7 @@ static void replace(char * target, const char * substring, const char * replacem
 	char * location = strstr(target, substring);
 	if (location)
 	{
-		buf2[0] = '\0';
-		strcat(buf2, replacement);
+		strcpy(buf2, replacement);
 		strcat(buf2, location + strlen(substring));
 		strcpy(target, buf2);
 	}
@@ -23,6 +22,7 @@ const char * roman_add(const char * a, const char * b)
 	strcpy(op1, a);
 	strcpy(op2, b);
 	replace(op1, "IV", "IIII");
+	replace(op2, "IV", "IIII");
 	strcpy(buffer, op1);
 	strcat(buffer, op2);
 	replace(buffer, "IIIII", "V");
