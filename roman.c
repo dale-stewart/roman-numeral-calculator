83bf480be445d8e0028490c358eefe66143e91b7
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 12:27:56 -0500

    Add forgotten extracted module

---
+++ roman.c
@@ -0,0 +1,6 @@
+#include "roman.h"
+
+const char * roman_add(const char * a, const char * b)
+{
+    return "II";
+}
---
+++ roman.h
@@ -0,0 +1,6 @@
+#ifndef __roman_h__
+#define __roman_h__
+
+const char * roman_add(const char * a, const char * b);
+
+#endif
