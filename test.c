37fab4040638b04d12d20646d60507b080ab3e2d
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 12:08:49 -0500

    Add first failing Roman numeral addition test

--- test.c
+++ test.c
@@ -2,6 +2,17 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
+const char * roman_add(const char * a, const char * b)
+{
+    return "";
+}
+
+START_TEST(one_plus_one_is_two)
+{
+    ck_assert_str_eq(roman_add("I", "I"), "II");
+}
+END_TEST
+
 START_TEST (true_is_true)
 {
     ck_assert(true);
@@ -23,13 +34,27 @@ Suite * test_suite(void)
     return s;
 }
 
+Suite * roman_suite(void)
+{
+    Suite * s;
+    TCase * tc;
+
+    s = suite_create("roman");
+    tc = tcase_create("roman_add");
+
+    tcase_add_test(tc, one_plus_one_is_two);
+    suite_add_tcase(s, tc);
+
+    return s;
+}
+
 int main(void)
 {
     int number_failed;
     Suite *s;
     SRunner *sr;
 
-    s = test_suite();
+    s = roman_suite();
     sr = srunner_create(s);
 
     srunner_run_all(sr, CK_NORMAL);
