1c072087784b8a32d7a1b8dd6a60b2bd5c8c8f9a
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 16:07:01 -0500

    Add failing XIV+I test

--- test.c
+++ test.c
@@ -45,6 +45,12 @@ START_TEST(one_plus_four_is_five)
 }
 END_TEST
 
+START_TEST(fourteen_plus_one_is_fifteen)
+{
+    ck_assert_str_eq(roman_add("XIV", "I"), "XV");
+}
+END_TEST
+
 Suite * roman_suite(void)
 {
     Suite * s;
@@ -60,6 +66,7 @@ Suite * roman_suite(void)
     tcase_add_test(tc, three_plus_three_is_six);
     tcase_add_test(tc, four_plus_one_is_five);
     tcase_add_test(tc, one_plus_four_is_five);
+    tcase_add_test(tc, fourteen_plus_one_is_fifteen);
 
     suite_add_tcase(s, tc);
 
