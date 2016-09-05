352654a21ab0670e228e5206231321f7669cd593
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 15:56:56 -0500

    Make IV+I test pass

--- roman.c
+++ roman.c
@@ -2,22 +2,30 @@
 #include "roman.h"
 
 static char buffer[32];
+static char op1[32];
+static char op2[32];
+static char buf2[32];
 
-static void replace_in_buffer(const char * substring, const char * replacement)
+static void replace(char * target, const char * substring, const char * replacement)
 {
-	char * location = strstr(buffer, substring);
+	char * location = strstr(target, substring);
 	if (location)
 	{
-		strcpy(location, replacement);
-		strcat(location, location + strlen(substring));
+		buf2[0] = '\0';
+		strcat(buf2, replacement);
+		strcat(buf2, location + strlen(substring));
+		strcpy(target, buf2);
 	}
 }
 
 const char * roman_add(const char * a, const char * b)
 {
-	strcpy(buffer, a);
-	strcat(buffer, b);
-	replace_in_buffer("IIIII", "V");
-	replace_in_buffer("IIII", "IV");
+	strcpy(op1, a);
+	strcpy(op2, b);
+	replace(op1, "IV", "IIII");
+	strcpy(buffer, op1);
+	strcat(buffer, op2);
+	replace(buffer, "IIIII", "V");
+	replace(buffer, "IIII", "IV");
     return buffer;
 }
