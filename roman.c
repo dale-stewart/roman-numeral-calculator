06dacf3697a91f039e3737dfb68cf50c1222c083
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 14:25:27 -0500

    Make III+III test pass

--- roman.c
+++ roman.c
@@ -3,13 +3,21 @@
 
 static char buffer[32];
 
+static void replace_in_buffer(const char * substring, const char * replacement)
+{
+	char * location = strstr(buffer, substring);
+	if (location)
+	{
+		strcpy(location, replacement);
+		strcat(location, location + strlen(substring));
+	}
+}
+
 const char * roman_add(const char * a, const char * b)
 {
 	strcpy(buffer, a);
 	strcat(buffer, b);
-	if (0 == strcmp(buffer, "IIII"))
-		strcpy(buffer, "IV");
-	if (0 == strcmp(buffer, "IIIII"))
-		strcpy(buffer, "V");
+	replace_in_buffer("IIIII", "V");
+	replace_in_buffer("IIII", "IV");
     return buffer;
 }
