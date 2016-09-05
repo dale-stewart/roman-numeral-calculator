2754db92195cc0d0243a3a08798982c06137b143
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-05 16:29:41 -0500

    Make I+XIV test pass

--- roman.c
+++ roman.c
@@ -23,6 +23,27 @@ static void replace(char * target, const char * substring, const char * replacem
 	}
 }
 
+static int compare_roman(const void * a, const void * b)
+{
+	const char * ca = (const char *)a;
+	const char * cb = (const char *)b;
+
+	const char * order = "IVXLCDM";
+
+	int index_a;
+	int index_b;
+
+	for (index_a = 0; index_a < strlen(order); ++index_a)
+		if (order[index_a] == *ca)
+			break;
+
+	for (index_b = 0; index_b < strlen(order); ++index_b)
+		if (order[index_b] == *cb)
+			break;
+
+	return index_b - index_a;
+}
+
 const char * roman_add(const char * a, const char * b)
 {
 	strcpy(op1, a);
@@ -31,6 +52,7 @@ const char * roman_add(const char * a, const char * b)
 	replace(op2, "IV", "IIII");
 	strcpy(buffer, op1);
 	strcat(buffer, op2);
+	qsort(buffer, strlen(buffer), sizeof(char), compare_roman);
 	replace(buffer, "IIIII", "V");
 	replace(buffer, "IIII", "IV");
     return buffer;
