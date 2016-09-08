f3f1667dd8b8b84c855af7de0af37ae2337dc7b3
Dale Stewart <dale.stewart@meresoftware.com>
2016-09-07 21:28:56 -0500

    Fix whitespace

--- roman.c
+++ roman.c
@@ -14,153 +14,153 @@ static bool replace(char * target, const char * substring, const char * replacem
 
 const char * roman_add(const char * a, const char * b)
 {
-	char rhs[32];
-	
-	strcpy(buffer, a);
-	denormalize(buffer);
+    char rhs[32];
 
-	strcpy(rhs, b);
-	denormalize(rhs);
-	strcat(buffer, rhs);
+    strcpy(buffer, a);
+    denormalize(buffer);
 
-	normalize(buffer);
+    strcpy(rhs, b);
+    denormalize(rhs);
+    strcat(buffer, rhs);
+
+    normalize(buffer);
 
     return buffer;
 }
 
 const char * roman_subtract(const char * a, const char * b)
 {
-	char rhs[32];
+    char rhs[32];
 
-	strcpy(buffer, a);
-	denormalize(buffer);
+    strcpy(buffer, a);
+    denormalize(buffer);
 
-	strcpy(rhs, b);
-	denormalize(rhs);
+    strcpy(rhs, b);
+    denormalize(rhs);
 
-	for (int i = 0; i < strlen(rhs); ++i)
-		subtract_one_symbol(buffer, rhs[i]);
+    for (int i = 0; i < strlen(rhs); ++i)
+        subtract_one_symbol(buffer, rhs[i]);
 
-	normalize(buffer);
+    normalize(buffer);
 
     return buffer;
 }
 
 static void denormalize(char * value)
 {
-	replace(value, "IV", "IIII");
-	replace(value, "IX", "VIIII");
-	replace(value, "XL", "XXXX");
-	replace(value, "XC", "LXXXX");
-	replace(value, "CD", "CCCC");
-	replace(value, "CM", "DCCCC");
+    replace(value, "IV", "IIII");
+    replace(value, "IX", "VIIII");
+    replace(value, "XL", "XXXX");
+    replace(value, "XC", "LXXXX");
+    replace(value, "CD", "CCCC");
+    replace(value, "CM", "DCCCC");
 }
 
 static void normalize(char * value)
 {
-	qsort(value, strlen(value), sizeof(char), compare_roman);
+    qsort(value, strlen(value), sizeof(char), compare_roman);
 
-	replace(value, "IIIII", "V");
-	replace(value, "IIII", "IV");
-	replace(value, "VIV", "IX");
-	replace(value, "VV", "X");
-	replace(value, "XXXXX", "L");
-	replace(value, "XXXX", "XL");
-	replace(value, "LXL", "XC");
-	replace(value, "LL", "C");
-	replace(value, "CCCCC", "D");
-	replace(value, "CCCC", "CD");
-	replace(value, "DCD", "CM");
-	replace(value, "DD", "M");
+    replace(value, "IIIII", "V");
+    replace(value, "IIII", "IV");
+    replace(value, "VIV", "IX");
+    replace(value, "VV", "X");
+    replace(value, "XXXXX", "L");
+    replace(value, "XXXX", "XL");
+    replace(value, "LXL", "XC");
+    replace(value, "LL", "C");
+    replace(value, "CCCCC", "D");
+    replace(value, "CCCC", "CD");
+    replace(value, "DCD", "CM");
+    replace(value, "DD", "M");
 }
 
 static int compare_roman(const void * a, const void * b)
 {
-	return roman_index(*(const char *)b) - roman_index(*(const char *)a);
+    return roman_index(*(const char *)b) - roman_index(*(const char *)a);
 }
 
 static int roman_index(char c)
 {
-	static const char order[] = {'I','V','X','L','C','D','M'};
-	static const int order_size = sizeof(order)/sizeof(order[0]);
+    static const char order[] = {'I','V','X','L','C','D','M'};
+    static const int order_size = sizeof(order)/sizeof(order[0]);
 
-	int index;
+    int index;
 
-	for (index = 0; index < order_size; ++index)
-		if (c == order[index])
-			break;
+    for (index = 0; index < order_size; ++index)
+        if (c == order[index])
+            break;
 
-	return index;
+    return index;
 }
 
 static void subtract_one_symbol(char * target, char symbol)
 {
-	char match[2];
+    char match[2];
 
-	match[0] = symbol;
-	match[1] = '\0';
+    match[0] = symbol;
+    match[1] = '\0';
 
-	if (!replace(target, match, ""))
-	{
-		if ('I' == symbol)
-		{
-			replace(target, "V", "IIII") ||
-			replace(target, "X", "VIIII") ||
-			replace(target, "L", "XXXXVIIII") ||
-			replace(target, "C", "LXXXXVIIII") ||
-			replace(target, "D", "CCCCLXXXXVIIII") ||
-			replace(target, "M", "DCCCCLXXXXVIIII");
-		}
-		else if ('V' == symbol)
-		{
-			replace(target, "X", "V") ||
-			replace(target, "L", "XXXXV") ||
-			replace(target, "C", "LXXXXV") ||
-			replace(target, "D", "CCCCLXXXXV") ||
-			replace(target, "M", "DCCCCLXXXXV");
-		}
-		else if ('X' == symbol)
-		{
-			replace(target, "L", "XXXX") ||
-			replace(target, "C", "LXXXX") ||
-			replace(target, "D", "CCCCLXXXX") ||
-			replace(target, "M", "DCCCCLXXXX");
-		}
-		else if ('L' == symbol)
-		{
-			replace(target, "C", "L") ||
-			replace(target, "D", "CCCCL") ||
-			replace(target, "M", "DCCCCL");
-		}
-		else if ('C' == symbol)
-		{
-			replace(target, "D", "CCCC") ||
-			replace(target, "M", "DCCCC");
-		}
-		else if ('D' == symbol)
-		{
-			replace(target, "M", "D");
-		}
-	}
+    if (!replace(target, match, ""))
+    {
+        if ('I' == symbol)
+        {
+            replace(target, "V", "IIII") ||
+            replace(target, "X", "VIIII") ||
+            replace(target, "L", "XXXXVIIII") ||
+            replace(target, "C", "LXXXXVIIII") ||
+            replace(target, "D", "CCCCLXXXXVIIII") ||
+            replace(target, "M", "DCCCCLXXXXVIIII");
+        }
+        else if ('V' == symbol)
+        {
+            replace(target, "X", "V") ||
+            replace(target, "L", "XXXXV") ||
+            replace(target, "C", "LXXXXV") ||
+            replace(target, "D", "CCCCLXXXXV") ||
+            replace(target, "M", "DCCCCLXXXXV");
+        }
+        else if ('X' == symbol)
+        {
+            replace(target, "L", "XXXX") ||
+            replace(target, "C", "LXXXX") ||
+            replace(target, "D", "CCCCLXXXX") ||
+            replace(target, "M", "DCCCCLXXXX");
+        }
+        else if ('L' == symbol)
+        {
+            replace(target, "C", "L") ||
+            replace(target, "D", "CCCCL") ||
+            replace(target, "M", "DCCCCL");
+        }
+        else if ('C' == symbol)
+        {
+            replace(target, "D", "CCCC") ||
+            replace(target, "M", "DCCCC");
+        }
+        else if ('D' == symbol)
+        {
+            replace(target, "M", "D");
+        }
+    }
 }
 
 static bool replace(char * target, const char * substring, const char * replacement)
 {
-	char temporary[32];
-	char * location = strstr(target, substring);
-	if (location)
-	{
-		temporary[0] = '\0';
-		if (location != target)
-		{
-			memcpy(temporary, target, location - target);
-			temporary[location - target] = 0;
-		}
-		strcat(temporary, replacement);
-		strcat(temporary, location + strlen(substring));
-		strcpy(target, temporary);
-		return true;
-	}
+    char temporary[32];
+    char * location = strstr(target, substring);
+    if (location)
+    {
+        temporary[0] = '\0';
+        if (location != target)
+        {
+            memcpy(temporary, target, location - target);
+            temporary[location - target] = 0;
+        }
+        strcat(temporary, replacement);
+        strcat(temporary, location + strlen(substring));
+        strcpy(target, temporary);
+        return true;
+    }
 
-	return false;
+    return false;
 }
