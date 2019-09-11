d5942b04de793194694418a74bd657f7ae3fd58b
Dale Stewart <dale.stewart@meresoftware.com>
2019-09-10 19:25:47 -0500

    Fix rule violations in exhaustive.c

--- exhaustive.c
+++ exhaustive.c
@@ -1,21 +1,23 @@
 #include "roman.h"
 #include <stdio.h>
 #include <string.h>
+#include <stdint.h>
 
 int main(void)
 {
-	char result[ROMAN_SIZE];
+    char result[ROMAN_SIZE];
 
-	strncpy(result, "I", sizeof(result) - 1);
-	result[sizeof(result) - 1] = '\0';
+    strncpy(result, "I", sizeof(result) - 1);
+    result[sizeof(result) - 1] = '\0';
 
-	int count = 3999;
+    int16_t count = 3999;
 
-	while (count--)
-	{
-		printf("%s\n", result);
-		roman_add(result, "I", result, sizeof(result));
-	}
+    do
+    {
+        printf("%s\n", result);
+        roman_add(result, "I", result, sizeof(result));
+    }
+    while (--count);
 
-	return 0;
+    return 0;
 }
