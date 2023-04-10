a103103a448551eca8c7612467e928c0273bd709
Dale Stewart <dale.stewart@meresoftware.com>
2023-04-10 08:41:15 -0500

    Address a security hotspot flagged by sonarcloud

--- exhaustive.c
+++ exhaustive.c
@@ -7,16 +7,20 @@ int main(void)
 {
     char result[ROMAN_SIZE];
 
-    (void)strncpy(result, "I", sizeof(result) - 1);
-    result[sizeof(result) - 1] = '\0';
+    result[sizeof result  - 1] = 0;
 
-    int16_t count = 3999;
+    (void)strncpy(result, "I", sizeof result);
 
-    while (count != 0)
+    if (result[sizeof result - 1] == 0)
     {
-        (void)printf("%s\n", result);
-        (void)romanAdd(result, "I", result, sizeof(result));
-        --count;
+        int16_t count = 3999;
+
+        while (count != 0)
+        {
+            (void)printf("%s\n", result);
+            (void)romanAdd(result, "I", result, sizeof(result));
+            --count;
+        }
     }
 
     return 0;
